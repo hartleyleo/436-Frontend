2605d5aff685299038ea88e491f3826e40c051bc
hartleyleo <hartleyleo2001@gmail.com>
2023-11-16 16:38:03 -0500

    Base files and light work

---
+++ my-app/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+
+
+    <div className="App">
+
+      <div className="Nav-bar">
+
+        <div className="Nav-button">Search</div>
+        <div className="Nav-button">Create</div>
+        <div className="Nav-button">Update</div>
+        <div className="Nav-button">Delete</div>
+
+      </div>
+
+      <header className="App-header">
+
+        <div className="Page-container">
+
+          <div className="Param-grid Page-element">
+            <div className="White-box">
+              <a>Paramaters:</a>
+              <a></a><input></input>
+            </div>
+          </div>
+          <div className="Results Page-element">
+            World
+          </div>
+
+          </div>
+
+
+      </header>
+    </div>
+
+
+
+
+  );
+}
+
+export default App;
