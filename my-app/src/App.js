59b38b9e4a8cf252c207a15b0e259d0d03278259
hartleyleo <hartleyleo2001@gmail.com>
2023-11-28 14:23:07 -0500

    Work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,52 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import Tabs from "./components/Tabs.js";
 
 function App() {
   return (
-
-
-
     <div className="App">
 
-      <div className="Nav-bar">
+      <Tabs>
+
+        
+
+          <div label="Search">
+            Search
+
+            {/* <div className="Page-container">
+
+              <div className="Param-grid Page-element">
+                <div className="White-box">
+
+                  <div className="Param-child Title">Search Paramaters:</div>
+                  <div className="Param-child Child-title">
+                    <a>User:</a>
+                    <input></input>
+                  </div>
+                  <div className="Param-child Child-title">
+                    <a>Table:</a>
+                    <input></input>
+                  </div>
+
+                </div>
+              </div>
+
+              <div className="Results Page-element White-box">
+                <div>Results</div>
+              </div>
+
+            </div> */}
+
+          </div>
+
+
+          <div label="Croc">
+            After 'while, <em>Crocodile</em>!
+          </div>
+
+          <div label="Sarcosuchus">
+            Nothing to see here, this tab is <em>extinct</em>!
+          </div>
+
+       
+
+      </Tabs>
+
+
+      {/* <div className="Nav-bar">
 
         <div className="Nav-button">Search</div>
         <div className="Nav-button">Create</div>
         <div className="Nav-button">Update</div>
         <div className="Nav-button">Delete</div>
 
-      </div>
+      </div> */}
 
-      <header className="App-header">
-
-        <div className="Page-container">
-
-          <div className="Param-grid Page-element">
-            <div className="White-box">
-
-              <div className="Param-child Title">Search Paramaters:</div>
-              <div className="Param-child">
-                <a>User:</a>
-                <input></input>
-              </div>
-              <div className="Param-child">
-                <a>Table:</a>
-                <input></input>
-              </div>
-
-              
-              
-
-            </div>
-          </div>
-          <div className="Results Page-element White-box">
-            <div>Results</div>
-          </div>
-
-          </div>
+      
 
 
-      </header>
+
+
     </div>
 
 
